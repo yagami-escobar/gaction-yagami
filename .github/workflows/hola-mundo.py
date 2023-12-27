0c235b2eb2e4701dd1490c6cec5031c24eddce3b
Jhordan Yagami <jordan19972010@gmail.com>
2023-12-26 19:24:17 -0500

    Update hola-mundo.py

--- .github/workflows/hola-mundo.py
+++ .github/workflows/hola-mundo.py
@@ -1,1 +1,8 @@
-print("Hello-World")
+import os  # Importo la libreria de Python os, permite acceder a env vars del os.
+
+def main():  # Creo la función main
+  nombre = os.getenv("USERNAME")
+  print(f"¡Hola, {nombre} desde Github!")
+
+if __name__ == "__main__"
+  main()
