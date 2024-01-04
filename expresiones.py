9780a9fcd5cd79b4f1d9c4dd28c11f16f04d38f6
Jhordan Yagami <jordan19972010@gmail.com>
2024-01-04 01:03:16 -0500

    Update expresiones.py

--- expresiones.py
+++ expresiones.py
@@ -4,7 +4,7 @@ def main():
   edad = os.getenv("EDAD")
   nombre = os.getenv("NOMBRE")
   lp = os.getenv("LP")
-  tag = os.getenv("TAG")
+  #tag = os.getenv("TAG")
   env = os.getenv("ENVIRONMENT")
   print(f"Hola {nombre} tienes {edad} años de edad tu LP favorito es {lp} y tu tag es {tag} y tu environment es {env}")
 
