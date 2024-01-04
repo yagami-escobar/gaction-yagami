32b42e976b72ac0270fb32c2b566d67cb32b02f5
Jhordan Yagami <jordan19972010@gmail.com>
2024-01-04 01:54:15 -0500

    Update expresiones.py

--- expresiones.py
+++ expresiones.py
@@ -4,7 +4,7 @@ def main():
   name = os.getenv('NAME')
   age = os.getenv('AGE')
   lp = os.getenv('LP')
-  sex = os.getemv('SEX')
+  sex = os.getenv('SEX')
   print(f"Hola tu nombre es {name} y tienes {age} años de edad y tu LP favorito es {lp} y tu sexo es {sex}")
 
 if __name__ == '__main__':
